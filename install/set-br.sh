98be856e4caecc104748d1b031eb7a949f4db52e
sehuadri <sehuadri@gmail.com>
2025-08-25 21:40:51 +0700

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/vip23-px/scnew/main/"
+REPO="https://raw.githubusercontent.com/sehuadri/scnew/main/"
 apt install rclone
 printf "q\n" | rclone config
 wget -O /root/.config/rclone/rclone.conf "${REPO}install/rclone.conf"
@@ -8,7 +8,7 @@ cd wondershaper
 make install
 cd
 rm -rf wondershaper
-wget -q ${REPO}install/limit.sh && chmod +x limit.sh && ./limit.sh
+wget -q ${REPO}install/limiter.sh && chmod +x limiter.sh && ./limiter.sh
     
 rm -f /root/set-br.sh
 rm -f /root/limit.sh
